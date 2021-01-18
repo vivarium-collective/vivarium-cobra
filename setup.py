ffc25d6193316e89b6919e9df21329e9104ec6eb
Eran Agmon <agmon.eran@gmail.com>
2021-01-18 11:07:23 -0800

    v0.0.4

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", 'r') as readme:
 
 setup(
     name='vivarium-cobra',
-    version='0.0.3',
+    version='0.0.4',
     packages=[
         'vivarium_cobra',
         'vivarium_cobra.processes',
