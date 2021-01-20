020acda1fab17cc64777c1ab2f0b0794ae316d80
Eran Agmon <agmon.eran@gmail.com>
2021-01-20 11:02:45 -0800

    starting cobra composite with derivers

---
+++ vivarium_cobra/composites/cobra_composite.py
@@ -0,0 +1,59 @@
+
+
+from vivarium.core.process import Composite
+from vivarium.processes.tree_mass import TreeMass
+from vivarium_cobra.processes.volume import Volume
+from vivarium_cobra.processes.dynamic_fba import DynamicFBA, get_iAF1260b_config
+
+
+
+class CobraComposite(Composite):
+
+    # set cobra constrained reactions
+    cobra_config = get_iAF1260b_config()
+
+    defaults = {
+        'cobra': cobra_config,
+        'flux_deriver': {},
+    }
+
+    def generate_processes(self, config):
+
+        cobra_process = DynamicFBA(config['cobra'])
+
+        return {
+            'cobra': cobra_process,
+            'mass_deriver': TreeMass(),
+            'volume_deriver': Volume(),
+        }
+
+    def generate_topology(self, config):
+        globals_path = ('globals',)
+
+        return {
+            'cobra': {
+                'internal_counts': ('internal_counts',),
+                'external': ('external',),
+                'exchanges': ('exchanges',),
+                'reactions': ('reactions',),
+                'flux_bounds': ('flux_bounds',),
+                'global': globals_path,
+            },
+            'mass_deriver': {
+                'global': globals_path,
+            },
+            'volume_deriver': {
+                'global': globals_path,
+            },
+        }
+
+
+def main():
+
+    import ipdb; ipdb.set_trace()
+
+
+
+
+if __name__ == '__main__':
+    main()
